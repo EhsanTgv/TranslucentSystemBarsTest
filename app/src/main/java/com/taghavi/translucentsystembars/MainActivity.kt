03cd29e0436d7ac1e86bd4979252ad43fcc02911
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-09 22:53:57 +0330

    implement recycler

--- app/src/main/java/com/taghavi/translucentsystembars/MainActivity.kt
+++ app/src/main/java/com/taghavi/translucentsystembars/MainActivity.kt
@@ -2,10 +2,30 @@ package com.taghavi.translucentsystembars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        moviesRecyclerView.apply {
+            addItemDecoration(
+                GridItemDecorator(
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.movies_divider
+                    )!!
+                )
+            )
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = MoviesAdapter(getTestData())
+        }
     }
 }
